aad08eb6b1fe5f90439ce4e01abdedc34ec94a4d
jvbmendes <kdobarcelos@gmail.com>
2024-03-19 17:12:03 -0300

    graficos

--- projeto/src/Home.jsx
+++ projeto/src/Home.jsx
@@ -3,10 +3,10 @@ import Graficos from "./Graficos"
 export const Home = () => {
     return (
         <>
-        <Graficos text="Graficos de linha" color= "lightpink"/>
-        <Graficos text="Graficos de area" color= "lightblue"/>
-        <Graficos text="Graficos de pizza" color= "lightyellow"/>
-        <Graficos text="Graficos de barra" color= "lightgreen"/>
+        <Graficos text="Graficos de linha" color= "lightpink" whatChart = "line"/>
+        <Graficos text="Graficos de area" color= "lightblue"  whatChart = "area"/>
+        <Graficos text="Graficos de pizza" color= "lightyellow"  whatChart = "line"/>
+        <Graficos text="Graficos de barra" color= "lightgreen"  whatChart = "line"/>
 
         </>
     )
